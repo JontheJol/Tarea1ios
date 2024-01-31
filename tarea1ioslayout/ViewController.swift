cc29c97ed5aacee2097924cb156804b708ef632e
jose issac brizuela giron <issac8713@hotmail.com>
2024-01-31 09:34:56 -0600

    Initial Commit

---
+++ tarea1ioslayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tarea1ioslayout
+//
+//  Created by Mac24 on 31/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
